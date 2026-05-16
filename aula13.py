43a4f70779bf84e7444e95203d188e53d52afefc
Mateus Dias <cptmert3@gmail.com>
2026-05-16 06:56:20 -0300

    mudanças nos nomes dos arquivos

---
+++ aula13.py
@@ -0,0 +1,9 @@
+disjuntor = input('digite (ligar) para ligar desligar a luz ou digite (desligar) para desligar a luz a luz: ')
+
+if disjuntor == 'ligar':
+    print('A luz ligou')
+elif  disjuntor == 'desligar':
+    print('A luz apagou')
+else:
+    print('Voce não digitou corretamente')
+print('fora do if ') # fora do bloco de codigos
